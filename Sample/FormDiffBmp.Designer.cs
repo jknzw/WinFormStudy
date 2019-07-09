3e7a44af61537f058a8c6786c356339c6fb46e8e
J.Kanazawa <j.kanazawa@obs-cop.net>
2019-07-09 22:42:26 +0900

    画像比較画面を追加

---
+++ Sample/FormDiffBmp.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Sample
+{
+    partial class FormDiffBmp
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new WindowsFormsControlLibrary.CustomButton();
+            this.button2 = new WindowsFormsControlLibrary.CustomButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(537, 12);
+            this.buttonSearch.Text = "比較";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Visible = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Visible = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(537, 41);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(597, 326);
+            this.pictureBox1.TabIndex = 100;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 101;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 101;
+            this.button2.Text = "参照";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(96, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 19);
+            this.textBox1.TabIndex = 102;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(96, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(307, 19);
+            this.textBox2.TabIndex = 102;
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxY1.Location = new System.Drawing.Point(491, 14);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(40, 19);
+            this.textBoxY1.TabIndex = 103;
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxX1.Location = new System.Drawing.Point(427, 14);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(40, 19);
+            this.textBoxX1.TabIndex = 103;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 12);
+            this.label1.TabIndex = 104;
+            this.label1.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(409, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 12);
+            this.label2.TabIndex = 104;
+            this.label2.Text = "X";
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxY2.Location = new System.Drawing.Point(491, 43);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(40, 19);
+            this.textBoxY2.TabIndex = 103;
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxX2.Location = new System.Drawing.Point(427, 43);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(40, 19);
+            this.textBoxX2.TabIndex = 103;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 12);
+            this.label3.TabIndex = 104;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 12);
+            this.label4.TabIndex = 104;
+            this.label4.Text = "X";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(15, 406);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(516, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 105;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Controls.SetChildIndex(this.buttonSearch, 0);
+            this.Controls.SetChildIndex(this.buttonClear, 0);
+            this.Controls.SetChildIndex(this.buttonUpdate, 0);
+            this.Controls.SetChildIndex(this.buttonDelete, 0);
+            this.Controls.SetChildIndex(this.buttonEnd, 0);
+            this.Controls.SetChildIndex(this.pictureBox1, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.textBoxY1, 0);
+            this.Controls.SetChildIndex(this.textBoxX1, 0);
+            this.Controls.SetChildIndex(this.textBoxY2, 0);
+            this.Controls.SetChildIndex(this.textBoxX2, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.progressBar1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private WindowsFormsControlLibrary.CustomButton button1;
+        private WindowsFormsControlLibrary.CustomButton button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ Sample/FormDiffBmp.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public partial class FormDiffBmp : Sample.BaseForm
+    {
+        public FormDiffBmp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            else
+            {
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog().Equals(DialogResult.OK))
+            {
+                textBox2.Text = openFileDialog2.FileName;
+            }
+            else
+            {
+            }
+        }
+
+        public override void SearchButton_Click(object sender, EventArgs e)
+        {
+            base.SearchButton_Click(sender, e);
+
+            Bitmap bmp1 = new Bitmap(textBox1.Text);
+            Bitmap bmp2 = new Bitmap(textBox2.Text);
+
+            int.TryParse(textBoxX1.Text, out int marginX1);
+            int.TryParse(textBoxY1.Text, out int marginY1);
+            int.TryParse(textBoxX2.Text, out int marginX2);
+            int.TryParse(textBoxY2.Text, out int marginY2);
+
+            int width = Math.Max(bmp1.Width + marginX1, bmp2.Width + marginX2);
+            int height = Math.Max(bmp1.Height + marginY1, bmp2.Height + marginY2);
+
+            Bitmap diffBmp = new Bitmap(width, height);         // 返却する差分の画像。
+            Color diffColor = Color.Red;                        // 画像の差分に付ける色。
+
+            progressBar1.Maximum = width;
+
+            // 全ピクセルを総当りで比較し、違う部分があればfalseを返す。
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (i + marginX1 >= bmp1.Width || i + marginX2 >= bmp2.Width
+                        || j + marginY1 >= bmp1.Height || j + marginY2 >= bmp2.Height)
+                    {
+                        // 縦横オーバー
+                        diffBmp.SetPixel(i, j, diffColor);
+                    }
+                    else
+                    {
+                        Color color1 = bmp1.GetPixel(i + marginX1, j + marginY1);
+                        if (color1 == bmp2.GetPixel(i + marginX2, j + marginY2))
+                        {
+                            diffBmp.SetPixel(i, j, color1);
+                        }
+                        else
+                        {
+                            diffBmp.SetPixel(i, j, diffColor);
+                        }
+                    }
+                }
+
+                progressBar1.Value = i;
+            }
+            //diffBmp.Save(path, ImageFormat.Png);
+            pictureBox1.Image = diffBmp;
+
+            progressBar1.Value = 0;
+        }
+    }
+}
